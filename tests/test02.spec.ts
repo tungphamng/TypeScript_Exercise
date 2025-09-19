b2d33549145ef4c72cfdc5c407a76b3413bcdd8a
tungphamng <tungphamng@gmail.com>
2025-09-19 16:36:42 +0700

    Push TC01 -02-03

---
+++ tests/test02.spec.ts
@@ -0,0 +1,46 @@
+import { test } from '@playwright/test';
+import { LoginPage} from '../pages/login.page';
+import { HomePage } from '../pages/home.page';
+import { ProductCategoryPage } from '../pages/product-category.page';
+import { CartPage } from '../pages/cart.page';
+import { CheckoutPage } from '../pages/checkout.page';
+import { OrderStatusPage } from '../pages/order-status.page';
+import { userInfo,billingInfo,products } from '../data/test-data.ts';
+import { PaymentMethod } from '../enum/data.enum.ts';
+
+
+test('TC_02 Verify users can buy multiple item successfully', async ({ page }) => {
+  // 1. Open browser and go to https://demo.testarchitect.com/
+  const homepage = new HomePage(page);
+  await homepage.goto();
+
+  // 2. Login with valid credentials 
+  await new LoginPage(page).login(userInfo.username, userInfo.password);
+
+  // 3. Go to Shop page
+  await homepage.gotoMenu('Shop');
+
+  // 4. Select multiple items and add to cart
+  await new ProductCategoryPage(page).addToCart(products);
+  await homepage.gotoCartPage();
+
+  // 5. Go to the cart and verify all selected items
+  const cartPage = new CartPage(page);
+  await cartPage.checkOrderItem(products);
+ 
+  // 6. Proceed to checkout and confirm order
+  await cartPage.proceedToCheckout();
+
+  const checkoutPage = new CheckoutPage(page);
+ 
+  await checkoutPage.ShouldOrderItemsDisplay(products);
+  
+  await checkoutPage.fillBillingInformation(billingInfo);
+  await checkoutPage.placeOrderButton.click();
+  
+  // 7. Verify order confirmation message
+  await new OrderStatusPage(page).verifyOrderDetails(products, billingInfo, PaymentMethod.DirectBankTransfer);
+
+});
+
+
